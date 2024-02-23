10d5a80ce3c39e63702bd30d10b3939840ce1bf5
Narayan Thakare <narayan111thakare@gmail.com>
2024-02-23 23:32:41 +0530

    fist commit

---
+++ Array.java
@@ -0,0 +1,21 @@
+
+public class Array {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		int a[]={1,23,6,67,4};
+		
+		int sum=0;
+		for (int i=0;i<a.length;i++){
+			
+			sum +=a[i];
+			
+		}
+		
+		System.out.println(sum);
+
+	}
+
+}
